f85268b7d8a218c0be8bd41740b6f2a5cc720d8c
David Guan <EcutDavid@users.noreply.github.com>
2017-04-03 12:36:20 +0800

    Implement basic styling (#4)
    * Implement basic mobile site
    
    * Implement card's desktop version
    
    * Add basic animation
    
    * Tweak styling

--- src/components/Main.js
+++ src/components/Main.js
@@ -9,43 +9,67 @@ import 'styles/main.scss';
 export default class AppComponent extends React.Component {
   constructor() {
     super();
-    this.state = { filter: '' };
+    // Should use deepcopy in real life application to prevent mockData src modified.
+    this.state = { filter: '', cards: mockData, presentedCardCount: 0 };
+  }
+
+  componentDidMount() {
+    let { cards, presentedCardCount } = this.state;
+
+    const presentCard = () => {
+      if (presentedCardCount !== cards.length) {
+        cards[presentedCardCount++].shouldPresent = true;
+        this.setState(cards);
+        setTimeout(presentCard, 100);
+      }
+    };
+    setTimeout(presentCard, 50);
+  }
+
+  updateFilter() {
+    const { value } = this.refs.filterInput;
+    if (value !== this.state.filter) {
+      this.setState({ filter: value });
+    }
   }
 
   keyHandle(event) {
     if (event.keyCode === KEYCODE_ENTER) {
-      const { value } = this.refs.filterInput;
-      if (value !== this.state.filter) {
-        this.setState({ filter: value });
-      }
+      this.updateFilter();
     }
   }
 
   filterCardsData() {
-    let { filter } = this.state;
+    let { filter, cards } = this.state;
     filter = filter.trim();
-    if (filter.trim() === '') return mockData;
+    if (filter.trim() === '') return cards;
 
-    return mockData.filter(({name, types}) => {
-      if (name.indexOf(filter) !== -1) return true;
-      if (types.some(d => d.indexOf(filter) !== -1)) return true;
+    const regex = new RegExp(filter, 'i');
+    return cards.filter(({name, types}) => {
+      if (regex.test(name)) return true;
+      if (types.some(d => regex.test(d))) return true;
     });
   }
 
   render() {
     return (
       <div className="index">
-        <input
-          type='text'
-          placeholder='input'
-          onKeyUp={this.keyHandle.bind(this)}
-          ref='filterInput'
-        />
-        {
-          this.filterCardsData().map((d, i) => (
-            <Card {...d} key={i} />
-          ))
-        }
+        <div className='filter-input'>
+          <input
+            type='text'
+            placeholder='Search coupons e.g. Amazon'
+            onKeyUp={this.keyHandle.bind(this)}
+            ref='filterInput'
+          />
+          <i className='search-icon' onClick={this.updateFilter.bind(this)} />
+        </div>
+        <div className='cards-container'>
+          {
+            this.filterCardsData().map((d, i) => (
+              <Card {...d} key={i} />
+            ))
+          }
+        </div>
       </div>
     );
   }
